ed7b404a37e20d025de8c8476666b704dc16fa05
fatfredyy <fatfredyy@fatfredyy.net>
2013-01-27 10:58:57 +0100

    Zmiany w stylach oraz dodanie oracle.

--- healthinformator-ejb/src/main/java/pl/healthinformator/model/HealthCareData.java
+++ healthinformator-ejb/src/main/java/pl/healthinformator/model/HealthCareData.java
@@ -6,12 +6,14 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @Entity
+@Table(name="SOA_HEALTH_CARE_DATA")
 public class HealthCareData implements Serializable {
 
 	private static final long serialVersionUID = -4250543534557893576L;
